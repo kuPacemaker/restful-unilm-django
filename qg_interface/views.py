56c2da975573040ea7077bba24b09e660eb63c39
binchoo <079111w@gmail.com>
2020-09-30 16:31:42 +0900

    Enable Json formatted bkd text.

--- qg_interface/views.py
+++ qg_interface/views.py
@@ -4,10 +4,10 @@ from rest_framework.response import Response
 from qg_interface.models import BaseKnowledge
 
 # Create your views here.
-@api_view(['GET', 'POST'])
+@api_view(['POST'])
 def question_generation(request):
-    if request.method == 'POST' or request.method == 'GET':
-        text = request.data
+    if request.method == 'POST':
+        text = request.data.text
         bkd = BaseKnowledge(text)
         bkd.attach_question()
         return Response(bkd.jsonate())
