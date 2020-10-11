fc24ded4ae1f3eb2ae956579a656a898ff33beb2
binchoo <jbinchoo@gmail.com>
2020-10-11 14:21:24 +0900

    Packagify

--- unilm_restful/settings.py
+++ unilm_restful/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'gvuryv$m47n5(^=j%8bu%@xqbi1pw__#%atj^rk3ti#5%eg16!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
