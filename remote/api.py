b7b6269c27bada629f40d27c99e4bfa34a42b49e
binchoo <079111w@gmail.com>
2020-10-11 14:15:52 +0900

    Refactor remote unilm service call interface.:

--- remote/api.py
+++ remote/api.py
@@ -1,12 +1,13 @@
-from .interface import nodes
+from remote.api.interface import nodes
 
 def call(query, service):
-    HOST, PORT = nodes[service], 
+    HOST, PORT = nodes[service]
+
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.settimeout(15.0)
     client_socket.connect((HOST, PORT))
+
     client_socket.sendall(query.encode())
-    
     recv = client_socket.recv(4096).decode('utf-8')
     client_socket.close()
     
