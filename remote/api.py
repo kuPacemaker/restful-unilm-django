36e24aaf0ae1046dc747ec3e45a7a374ec53f343
binchoo <079111w@gmail.com>
2020-10-21 21:57:54 +0900

    Change name _parse to parse_protocol.

--- remote/api.py
+++ remote/api.py
@@ -1,7 +1,7 @@
 import socket
 
 def call(protocol):
-    HOST, PORT, QUERY, TIMEOUT = _parse(protocol)
+    HOST, PORT, QUERY, TIMEOUT = parse_protocol(protocol)
 
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.settimeout(TIMEOUT)
@@ -14,7 +14,7 @@ def call(protocol):
     response = recv.split('\n')
     return response
 
-def _parse(protocol):
+def parse_protocol(protocol):
     HOST, PORT = protocol.node
     QUERY = protocol.gen_query().encode()
     TIMEOUT = protocol.timeout
