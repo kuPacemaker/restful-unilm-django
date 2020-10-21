197d42447391f660d680921ddb44a9d091bca581
binchoo <079111w@gmail.com>
2020-10-21 15:09:13 +0900

    Add protocols.py, QAProtocol, QGProtocol

---
+++ remote/protocols.py
@@ -0,0 +1,14 @@
+
+class QAProtocol:
+
+    __node__ = ('117.16.136.170', 3421)
+
+    def gen_query(self, bkd):
+        pass
+
+class QGProtocol:
+
+    __node__ = ('117.16.136.171', 2593)
+
+    def gen_query(self, bkd):
+        pass
