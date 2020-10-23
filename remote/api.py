08896b5af5804315b334cf90d7fb6a9ca7209896
binchoo <079111w@gmail.com>
2020-10-23 12:05:22 +0900

    Fix call function.

--- remote/api.py
+++ remote/api.py
@@ -3,14 +3,15 @@ import socket
 def call(protocol):
     HOST, PORT, QUERY, TIMEOUT = protocol.parse()
     csock = setup_socket_connection(HOST, PORT, timeout)
-    response = []
+
     for each_query in QUERY:
         csock.sendall(each_query)
         res = csock.recv(4096).decode('utf-8')
-        response.append(res.split('\n'))
+        res = res.split('\n')
+        protocol.notify_response(res)
+        
     csock.close()
-    protocol.notify_response(response)
-    return response
+    
 
 def setup_socket_connection(host, port, timeout):
     csock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
