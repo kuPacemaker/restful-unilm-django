b3dc7d9c75c159613867dbe21c2b80e5462f678d
binchoo <079111w@gmail.com>
2020-10-25 12:51:25 +0900

    Add cache config.

--- unilm_restful/settings.py
+++ unilm_restful/settings.py
@@ -81,6 +81,12 @@ DATABASES = {
     }
 }
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': 'unix:/tmp/memcached.sock',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
