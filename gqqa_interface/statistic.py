7d236a0456b69fd9b2e44bdd8a3fd87ae4323888
binchoo <079111w@gmail.com>
2020-10-25 22:56:52 +0900

    Set index.

--- gqqa_interface/statistic.py
+++ gqqa_interface/statistic.py
@@ -37,9 +37,9 @@ class GQQAHistory:
 
     def save_csv(self):
         if not os.path.exists(self.csv_file):
-            self.df.to_csv(self.csv_file, mode='w', index=False)
+            self.df.to_csv(self.csv_file, mode='w', index=True)
         else:
-            self.df.to_csv(self.csv_file, mode='a', header=False, index=False)
+            self.df.to_csv(self.csv_file, mode='a', header=False, index=True)
         return self.df
 
     def clear(self):
