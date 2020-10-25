40286302c9e7264c9e2d602e4160794c94841c87
binchoo <079111w@gmail.com>
2020-10-25 12:23:10 +0900

    Add GQQAStatistic class that records request and response of QGGA service during one server lifetime.

---
+++ gqqa_interface/statistic.py
@@ -0,0 +1,34 @@
+import pandas
+from base import BaseKnowledge
+
+class GQQAStatistic:
+
+    def __init__(self):
+        self.clear()
+
+    def clear(self):
+        self.history = self._init_data()
+
+    def _init_data(self):
+        return {
+            'Paragraph': [],
+            'Extracted Answer': [], 'Generated Question': [], 'Generated Answer': []
+        }
+
+    def add_qa_result(self, bkd: BaseKnowledge):
+        for passage in bkd.passages:
+            for answer, _ in passage.aqset:
+                self.history['Generated Answer'].append(answer)
+
+    def add_qg_result(self, bkd: BaseKnowledge):
+        for passage in bkd.passages:
+            for answer, question in passage.aqset:
+                self.history['Paragraph'].append(passage.text)
+                self.history['Extracted Answer'].append(answer)
+                self.history['Generated Question'].append(question)
+
+    def to_dataframe(self):
+        return pandas.DataFrame(self.data)
+
+    def to_html(self):
+        return self.to_dataframe().df.to_html()
--- gqqa_interface/views.py
+++ gqqa_interface/views.py
@@ -2,20 +2,21 @@ from django.shortcuts import render
 from django.response import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-import pandas
 
 import remote.api as RemoteApi
 from base import BaseKnowledge
-from .protocol import GQQAProtocol
 from qg_interface.protocol import QGProtocol
+from .protocol import GQQAProtocol
+from .statistic import GQQAStatistic
 
 # Create your views here.
-history: list = []
+statistic = GQQAStatistic()
 @api_view(['POST'])
 def answer_generation_for_generated_question(request):
     if request.method == 'POST':
         bkd = BaseKnowledge(request.data['bkd'])
         RemoteApi.call(QGProtocol(bkd))
+        
         RemoteApi.call(GQQAProtocol(bkd))
         return Response(bkd.jsonate())
     return Response({"message": "The %s method is not appropriate." % request.method})
@@ -24,9 +25,8 @@ gqqa = answer_generation_for_generated_question
 @api_view(['GET', 'DELETE'])
 def gqqa_request_history(request):
     if request.method == 'DELETE':
-        pass
+        statistic.clear()
     elif request.method == 'GET':
-        history
-        return HttpResponse()
+        return HttpResponse(statistic.to_html())
     return Response({"message": "The %s method is not appropriate." % request.method})
 gqqa_history = gqqa_request_history
