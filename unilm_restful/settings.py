84ecaeae636c87732315164edf1de908e1d4fe9a
binchoo <079111w@gmail.com>
2020-10-25 19:12:08 +0900

    Remove other whitelist excep pacemaker

--- unilm_restful/settings.py
+++ unilm_restful/settings.py
@@ -131,8 +131,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_WHITELIST = [
-    'google.com',
-    'localhost:8000',
-    'github.io',
     'https://kupacemaker.github.io'
 ]
