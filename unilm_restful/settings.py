65fb0d63292954360d6ee2cbb3e77a7d202eaee2
binchoo <079111w@gmail.com>
2020-10-25 19:07:31 +0900

    Enable cross-origin headers.

--- unilm_restful/settings.py
+++ unilm_restful/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corshearders',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'unilm_restful.urls'
@@ -126,3 +128,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = [
+    'google.com',
+    'localhost:8000',
+    'github.io',
+    'kupacemaker.github.io'
+]
