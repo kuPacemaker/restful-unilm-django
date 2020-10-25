d58c6687f090f5a12d396f93118b8e0fd3297fa3
binchoo <079111w@gmail.com>
2020-10-25 13:00:17 +0900

    Use cache to remember dataframe html page.

--- gqqa_interface/views.py
+++ gqqa_interface/views.py
@@ -35,7 +35,7 @@ def gqqa_request_history(request):
     if request.method == 'DELETE':
         history.clear()
     elif request.method == 'GET':
-        return HttpResponse(with_style(cache['history-html']))
+        return HttpResponse(with_style(cache.get('history-html')))
     return Response({"message": "The %s method is not appropriate." % request.method})
 
 def with_style(html):
