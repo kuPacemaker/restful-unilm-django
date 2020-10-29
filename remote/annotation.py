93369a7bb6b1b276702fe5f1d8056491b0155340
binchoo <079111w@gmail.com>
2020-10-29 18:33:46 +0900

    Use self

--- remote/annotation.py
+++ remote/annotation.py
@@ -1,7 +1,8 @@
 def terminate(final):
     def query_terminator(query_generator):
         def decorated(*args, **kwargs):
-            for query in query_generator():
+            self = args[0]
+            for query in query_generator(self):
                 yield query + final
         return decorated
     return query_terminator
