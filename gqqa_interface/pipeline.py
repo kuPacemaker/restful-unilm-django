a1a52175074932b4e2193602ddeb86326de283e1
binchoo <079111w@gmail.com>
2020-10-29 20:44:54 +0900

    Use condition's notify.

--- gqqa_interface/pipeline.py
+++ gqqa_interface/pipeline.py
@@ -71,7 +71,7 @@ class PipelineUnit(metaclass=ABCMeta):
 
     def enqueue(self, item):
         self.queue.append(item)
-        #self.th.notify()
+        self.condition.notify()
 
     @abstractmethod
     def process(self, item):
