cd40ba9eecb89b91eea181067bc03ba40e8446b4
binchoo <079111w@gmail.com>
2020-10-29 18:32:33 +0900

    Remove qgga protocol in qg_interface/protocol.py

--- qa_interface/protocol.py
+++ qa_interface/protocol.py
@@ -29,31 +29,3 @@ class QAProtocol(AbstractProtocol):
 
     def protocol_reset(self):
         self.response_attach_head = 0
-
-class GQQAProtocol(AbstractProtocol):
-    node = ('117.16.136.170', 3421)
-
-    def __init__(self, bkd: BaseKnowledge):
-        self.bkd = bkd
-        self.questions = None
-        self.response_attach_head = 0
-
-    @terminate(self.TERMINATOR)
-    def gen_query(self):
-        for passage in self.bkd.passages:
-            text = passage.text
-            aqset = passage.aqset
-            passage.prev_aqset = aqset
-            query = ''
-
-            self.questions = list(map(lambda x: x[1], aqset))
-            for answer, question in aqset:
-                query += self.sep(text, question)
-            yield query
-
-    def notify_response(self, response):
-        self.bkd.attach_aqset(self.response_attach_head, list(zip(response, self.questions)))
-        self.response_attach_head += 1
-
-    def protocol_reset(self):
-        self.response_attach_head = 0
