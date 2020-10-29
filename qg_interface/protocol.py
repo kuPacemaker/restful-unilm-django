54744d075e94be7dc7f0352f16b2306c2c036ff5
binchoo <079111w@gmail.com>
2020-10-29 15:39:26 +0900

    Set qg_protocol default value as None

--- qg_interface/protocol.py
+++ qg_interface/protocol.py
@@ -4,7 +4,7 @@ from base import BaseKnowledge
 class QGProtocol(AbstractProtocol):
     node = ('117.16.136.171', 2593)
 
-    def __init__(self, bkd: BaseKnowledge, num_case=3):
+    def __init__(self, bkd: BaseKnowledge, num_case=None):
         self.bkd = bkd
         self.num_case = num_case if num_case is not None else 3 + int(3/len(self.bkd.passages))
         self.answers = None
