be19353795e3dc2f1ba604a4ae499e2d96b14d39
binchoo <079111w@gmail.com>
2020-10-30 14:36:44 +0900

    Use QA larger context.

--- qa_interface/views.py
+++ qa_interface/views.py
@@ -10,7 +10,7 @@ from .protocol import QAProtocol
 @api_view(['POST'])
 def answer_generation(request):
     if request.method == 'POST':
-        bkd = BaseKnowledge(request.data['bkd'], 1000)
+        bkd = BaseKnowledge(request.data['bkd'], 600)
         q = request.data['q']
         RemoteApi.call(QAProtocol(bkd, question=q))
         return Response(bkd.jsonate())
