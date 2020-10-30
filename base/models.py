fc68adc2f16027a52e48f6f36b59441ac5e34b4c
binchoo <079111w@gmail.com>
2020-10-30 18:03:16 +0900

    Add prune_passage, which remains only one passage with top_score

--- base/models.py
+++ base/models.py
@@ -17,5 +17,12 @@ class BaseKnowledge:
     def attach_aqset(self, index, aqset):
         self.passages[index].aqset = aqset
 
+    def prune_passage(self):
+        for i, passage in enumerate(self.passages):
+            _, _, score = passage.aqset[0]
+            if i == 0 or (i > 0 and top_score < score):
+                top_passage, top_score = passage, score
+        self.passages = [top_passage]
+        
     def jsonate(self):
         return json.loads(json.dumps(self, default=lambda o: o.__dict__, indent = 4))
--- qa_interface/views.py
+++ qa_interface/views.py
@@ -12,6 +12,9 @@ def answer_generation(request):
     if request.method == 'POST':
         bkd = BaseKnowledge(request.data['bkd'], 350)
         q = request.data['q']
+
         RemoteApi.call(QAProtocol(bkd, question=q, num_case=1))
+        bkd.prune_passage()
+        
         return Response(bkd.jsonate())
     return Response({"message": "The GET method is not appropriate."})
