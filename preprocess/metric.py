c80c95b50ef865229f6a7dcd5e67276a4a5a8ecc
binchoo <079111w@gmail.com>
2020-11-02 23:05:37 +0900

    Fix typo

--- preprocess/metric.py
+++ preprocess/metric.py
@@ -10,7 +10,7 @@ class SquadTfIdf:
 
     def transform(self, docs):
         vectors = self.vectorizer.transform(docs)
-        words = self.inverse_transform(vectors)
+        words = self.vectorizer.inverse_transform(vectors)
 
         tfidfs = []
         for doc_id, vector in enumerate(vectors):
@@ -22,17 +22,17 @@ class SquadTfIdf:
             tfidfs.append(tfidf)
         return tfidfs
 
-class TfIdfLen:
-    
-    def __init__(self, word_lists):
-        self.idf = dict()
-        self.num_docs = max(len(word_lists), 2)
+# class TfIdfLen:
 
-        for i, word_list in enumerate(word_lists):
-            for noun in word_list:
-                self.idf[noun] = i+1
+#     def __init__(self, word_lists):
+#         self.idf = dict()
+#         self.num_docs = max(len(word_lists), 2)
+
+#         for i, word_list in enumerate(word_lists):
+#             for noun in word_list:
+#                 self.idf[noun] = i+1
     
-    def fit_transform(self, word_list):
-        tf = Counter(word_list)
-        tfidf_score = {noun: tf_score * math.log(self.num_docs / self.idf[noun]) * math.log(len(noun)) / math.log(3.3) for noun, tf_score in tf.items()}
-        return tfidf_score
+#     def fit_transform(self, word_list):
+#         tf = Counter(word_list)
+#         tfidf_score = {noun: tf_score * math.log(self.num_docs / self.idf[noun]) * math.log(len(noun)) / math.log(3.3) for noun, tf_score in tf.items()}
+#         return tfidf_score
